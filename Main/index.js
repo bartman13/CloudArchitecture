b0d2d39804c7ea248f72d2f5eb9666537e89a059
Piotr Kryczka <pmk.poczta@gmail.com>
2021-05-24 22:03:55 +0200

    add filters

--- Main/index.js
+++ Main/index.js
@@ -3,12 +3,13 @@ const https = require('follow-redirects').https,
 
 let baseNasaUrl = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
 
-const getImages = function(day, callback) {
+const getImages = function(day, callback, filterName) {
+    var filterArgs = Array.prototype.slice.call(arguments, 3);
     let requestUrl = prepareRequestUrl(day)
     https.get(requestUrl, (res) => {
         let body = '';
         res.on('data', chunk => body += chunk);
-        res.on('end', () => callback(null, JSON.parse(body)));
+        res.on('end', () => callback(null, JSON.parse(body), filterName, filterArgs));
     }).on('error', (e) => {
         callback(Error(e))
     })
@@ -25,22 +26,25 @@ const prepareRequestUrl = function(day) {
     return url
 }
 
-const extractImageUrls = function(_, body) {
+const extractImageUrls = function(_, body, filterName, filterArgs) {
     let urls = []
     body.photos.forEach(function(photo) {
         urls.push(photo.img_src.replace("http", "https"))
     })
     console.log(urls)
-    downloadImages(urls)
+    downloadAndFilterImages(urls, filterName, filterArgs)
 }
 
-const downloadImages = function(urls) {
+const downloadAndFilterImages = function(urls, filterName, filterArgs) {
+    console.log(filterArgs)
+    const filterArgsObj = Object.fromEntries(filterArgs);
+    console.log(filterArgsObj)
     urls.forEach(function(url) {
         const dest = url.split("/").pop()
         console.log(url)
         let file = fs.createWriteStream(dest);
         https.get(url, function(response) {
-            response.pipe(file);
+            response.pipe(executeFunctionByName(filterName, sharp(), filterArgsObj)).pipe(file);
             file.on('finish', function() {
                 file.close();
             });
@@ -48,4 +52,17 @@ const downloadImages = function(urls) {
     })
 }
 
-getImages("2015-6-3", extractImageUrls)
+const sharp = require('sharp');
+
+function executeFunctionByName(functionName, context /*, args */) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    var namespaces = functionName.split(".");
+    var func = namespaces.pop();
+    for(var i = 0; i < namespaces.length; i++) {
+        context = context[namespaces[i]];
+    }
+    return context[func].apply(context, args);
+}
+
+//getImages("2015-6-3", extractImageUrls, "negate")
+getImages("2015-6-3", extractImageUrls, "modulate", ["brightness", 0.5], ["saturation", 0.5], ["hue", 90])
