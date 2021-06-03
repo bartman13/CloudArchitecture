78154220b78253cfaaf0537cb100155479d72dfb
Jakub Michalak <michalakjakub1999@gmail.com>
2021-06-03 17:05:24 +0200

    save to s3 and cleanup (#3)

--- Main/lambda/index.js
+++ Main/lambda/index.js
@@ -11,28 +11,28 @@ var AWS = require('aws-sdk');
 
 
 exports.handler = (event, context, callback) => {
-    
+
   // process GET request
   if(event.queryStringParameters){
     var queryParams = event.queryStringParameters;
- 
+
     var rVerifyToken = queryParams['hub.verify_token']
     if (rVerifyToken === VERIFY_TOKEN) {
       var challenge = queryParams['hub.challenge'];
-      const fetch = require("node-fetch");
-      fetch('https://salesystemapi.azurewebsites.net/categories').then((response) => callback(null, response));
     }
-  
+
   // process POST request
   }else{
     var data = JSON.parse(event.body);
-     
+
     // Make sure this is a page subscription
     if (data.object === 'page') {    // Iterate over each entry - there may be multiple if batched
     data.entry.forEach(function(entry) {
+        console.log("data entry")
         var pageID = entry.id;
         var timeOfEvent = entry.time;        // Iterate over each messaging event
         entry.messaging.forEach(function(msg) {
+          console.log("entry messaging")
           if (msg.message) {
             receivedMessage(msg);
           } else {
@@ -40,7 +40,7 @@ exports.handler = (event, context, callback) => {
           }
         });
     });
-    
+
     }    // Assume all went well.
     //
     // You must send back a 200, within 20 seconds, to let us know
@@ -50,7 +50,7 @@ exports.handler = (event, context, callback) => {
       'body': "ok",
       'statusCode': 200
     };
-      
+
     callback(null, response);
   }
 }
@@ -104,14 +104,14 @@ function callSendAPI(messageData) {
       str += chunk;
     });
     response.on('end', function () {
- 
+
     });
   }
   var req = https.request(options, callback);
   req.on('error', function(e) {
     console.log('problem with request: '+ e);
   });
- 
+
   req.write(body);
   req.end();
 }
@@ -128,9 +128,9 @@ const getImages = function(day, callback, filterName) {
     var filterArgs = Array.prototype.slice.call(arguments, 3);
     let requestUrl = prepareRequestUrl(day)
     httpsFR.get(requestUrl, (res) => {
-        let body = '';
-        res.on('data', chunk => body += chunk);
-        res.on('end', () => callback(null, JSON.parse(body), filterName, filterArgs));
+      let body = '';
+      res.on('data', chunk => body += chunk);
+      res.on('end', () => callback(null, JSON.parse(body), filterName, filterArgs));
     }).on('error', (e) => {
         callback(Error(e))
     })
@@ -150,32 +150,76 @@ const extractImageUrls = function(_, body, filterName, filterArgs) {
     downloadAndFilterImages(urls, filterName, filterArgs)
 }
 
-const putObjectToS3 = function(bucket, key, data) {
+const putObjectToS3 = async function(bucket, key, data) {
   var s3 = new AWS.S3({
     apiVersion: '2006-03-01',
     params: {Bucket: bucket}
   });
   var params = {
-	  Bucket: bucket,
+      Bucket : bucket,
       Key : key,
       Body : data
   }
-  console.log(s3)
   console.log("DATA: ", data)
-  s3.upload(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log('SUCCESS:' + data);           // successful response
-  });
+  const resp = await s3.upload({Bucket: bucket, Key: key, Body: data}).promise();
+  console.log("RESP: ", resp)
+  return {
+    statusCode: 200,
+    body: JSON.stringify(resp)
+  }
 }
+
+// async function upload(file) {
+
+// // Create S3 service object
+// var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+// // call S3 to retrieve upload file to specified bucket
+// var uploadParams = {Bucket: 'cloud-project-mars-photos', Key: '', Body: ''};
+
+// // Configure the file stream and obtain the upload parameters
+// var fs = require('fs');
+// var fileStream = fs.createReadStream(file);
+// fileStream.on('error', function(err) {
+//   console.log('File Error', err);
+// });
+// uploadParams.Body = fileStream;
+// var path = require('path');
+// uploadParams.Key = path.basename('a.jpg');
+
+// // call S3 to retrieve upload file to specified bucket
+// s3.upload (uploadParams, function (err, data) {
+//   if (err) {
+//     console.log("Error", err);
+//   } if (data) {
+//     console.log("Upload Success", data.Location);
+//   }
+// });
+
+// }
 
 const downloadAndFilterImages = function(urls, filterName, filterArgs) {
     const filterArgsObj = Object.fromEntries(filterArgs);
     urls.forEach(function(url) {
+        const dest = url.split("/").pop()
         console.log(url)
-        let response2 = httpsFR.get(url, function(response) {
-            response.pipe(executeFunctionByName(filterName, sharp(), filterArgsObj));
-        })
-        putObjectToS3('cloud-project-mars-photos','file1.jpg', response2)
+
+        httpsFR.get(url, (res) => {
+          const data = [];
+          res.on('data', (chunk) => {
+            data.push(chunk);
+          }).on('end', () => {
+            let buffer = Buffer.concat(data);
+            // Do something with the buffer
+            let r = Math.random().toString(36).substring(7);
+            putObjectToS3('cloud-project-mars-photos','file' + filterName + r + '.jpg', buffer)
+          });
+        }).on('error', (err) => {
+          console.log('download error:', err);
+        });
+      	//let r = Math.random().toString(36).substring(7);
+        //putObjectToS3('cloud-project-mars-photos','file' + filterName + r + '.jpg', response2)
+        //upload(response2)
     })
 }
 
@@ -193,3 +237,15 @@ function executeFunctionByName(functionName, context ) {
 
 //getImages("2015-6-3", extractImageUrls, "negate")
 //getImages("2015-6-3", extractImageUrls, "modulate", ["brightness", 0.5], ["saturation", 0.5], ["hue", 90])
+
+ function stream2buffer( stream ) {
+
+        return new Promise( (resolve, reject) => {
+            let _buf = []
+
+            stream.on( 'data', chunk => _buf.push(chunk) )
+            stream.on( 'end', () => resolve(Buffer.concat(_buf)) )
+            stream.on( 'error', err => reject( err ))
+
+        })
+ }
